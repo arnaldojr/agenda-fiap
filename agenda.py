a616969e46bef05e65222dbe2cc96d054719fde6
arnaldojr <arnaldo_viana@yahoo.com.br>
2023-10-10 12:06:24 -0300

    def menu

--- agenda.py
+++ agenda.py
@@ -66,51 +66,55 @@ def buscar_contato(agenda, nome):
     print(agenda.get(nome, "contato não existe"))
 
 
+def menu():
+    while True:
+        print("\n__agenda de contatos__\n")
+        print(" 1. - adicionar contato")
+        print(" 2. - listar contato")
+        print(" 3. - editar contato")
+        print(" 4. - excluir contato")
+        print(" 5. - buscar contato")
+        print(" 6. - sair\n")   
+        user_op = input("Escolha uma opção:")
 
-while True:
-    print("\n__agenda de contatos__\n")
-    print(" 1. - adicionar contato")
-    print(" 2. - listar contato")
-    print(" 3. - editar contato")
-    print(" 4. - excluir contato")
-    print(" 5. - buscar contato")
-    print(" 6. - sair\n")   
-    user_op = input("Escolha uma opção:")
+        if user_op == "1":
+            user_name = input("Nome do contato:")
+            if user_name not in agenda:
+                user_tel = input("Tel do contato:")
+                user_email = input("email do contato:")
+                user_end = input("end do contato:")
+                adicionar_contato(agenda,user_name,user_tel,user_email,user_end)
+            else:
+                print(f"Contato {user_name} já existe!")
 
-    if user_op == "1":
-        user_name = input("Nome do contato:")
-        if user_name not in agenda:
-            user_tel = input("Tel do contato:")
-            user_email = input("email do contato:")
-            user_end = input("end do contato:")
-            adicionar_contato(agenda,user_name,user_tel,user_email,user_end)
+        elif user_op == "2":
+            listar_contato(agenda)
+
+        elif user_op == "3":
+            user_name = input("Nome do contato:")
+            if user_name in agenda:
+                user_tel = input("Tel do contato:")
+                user_email = input("email do contato:")
+                user_end = input("end do contato:")
+                editar_contato(agenda,user_name,user_tel,user_email,user_end)
+            else:
+                print(f"Contato {user_name} não existe!")
+
+        elif user_op == "4":
+            user_name = input("Escolha o contato para excluir:")
+            excluir_contato(agenda,user_name)
+
+        elif user_op == "5":
+            user_name = input("Encontre o contato:")
+            buscar_contato(agenda,user_name)
+
+        elif user_op == "6":
+            print("Obrigado, fechando o programa!!")
+            break
+
         else:
-            print(f"Contato {user_name} já existe!")
+            print(f"opção {user_op} invalida! Escolha uma opção valida.")      
 
-    elif user_op == "2":
-        listar_contato(agenda)
 
-    elif user_op == "3":
-        user_name = input("Nome do contato:")
-        if user_name in agenda:
-            user_tel = input("Tel do contato:")
-            user_email = input("email do contato:")
-            user_end = input("end do contato:")
-            editar_contato(agenda,user_name,user_tel,user_email,user_end)
-        else:
-            print(f"Contato {user_name} não existe!")
-
-    elif user_op == "4":
-        user_name = input("Escolha o contato para excluir:")
-        excluir_contato(agenda,user_name)
-
-    elif user_op == "5":
-        user_name = input("Encontre o contato:")
-        buscar_contato(agenda,user_name)
-
-    elif user_op == "6":
-        print("Obrigado, fechando o programa!!")
-        break
-
-    else:
-        print(f"opção {user_op} invalida! Escolha uma opção valida.")      
+if __name__ == "__main__":
+    menu()
