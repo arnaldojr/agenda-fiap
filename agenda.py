2b37591c8b5afeb4463641983123dfd1228f69fe
arnaldojr <arnaldo_viana@yahoo.com.br>
2023-10-10 11:39:42 -0300

    adicionar

--- agenda.py
+++ agenda.py
@@ -16,17 +16,23 @@ agenda = {
     "pedro":{
         "telefone":"22258-6998",
         "email": "pedro@email.com",
-        "endeço": "rua pedro, 123"
+        "endereço": "rua pedro, 123"
     },
     "renato":{
-        "telefone":"22258-6998",
-        "email": "pedro@email.com",
-        "endeço": "rua pedro, 123"
+        "telefone":"9999666332",
+        "email": "renato@email.com",
+        "endereço": "rua renato, 123"
     } 
 }
 
-def adicionar_contato():
-    pass
+def adicionar_contato(agenda,nome,tel,email,end):
+    novo_contato = {}
+    novo_contato["telefone"] = tel
+    novo_contato["email"] = email
+    novo_contato["endereço"] = end
+    
+    agenda[nome]= novo_contato
+    print(f"contato {nome} adicionado com sucesso!!!")
 def listar_contato():
     pass
 def editar_contato():
@@ -40,13 +46,10 @@ def excluir_contato(agenda,nome):
         print(f"Contato {nome} não existe!")
     # forma 2
     #del agenda.get(nome, "não existe")
-    
 
+def buscar_contato(agenda, nome):
+    print(agenda.get(nome, "contato não existe"))
 
-
-
-def buscar_contato():
-    pass
 
 
 while True:
@@ -58,22 +61,31 @@ while True:
     print(" 5. - buscar contato")
     print(" 6. - sair\n")   
     user_op = input("Escolha uma opção:")
+
     if user_op == "1":
-        print("opção1")
+        user_name = input("Nome do contato:")
+        user_tel = input("Tel do contato:")
+        user_email = input("email do contato:")
+        user_end = input("end do contato:")
+        adicionar_contato(agenda,user_name,user_tel,user_email,user_end)
+
     elif user_op == "2":
         print("opção2")
+
     elif user_op == "3":
         print("opção3")
+
     elif user_op == "4":
         user_name = input("Escolha o contato para excluir:")
         excluir_contato(agenda,user_name)
 
-
-
     elif user_op == "5":
-        print("opção5")  
+        user_name = input("Encontre o contato:")
+        buscar_contato(agenda,user_name)
+
     elif user_op == "6":
         print("Obrigado, fechando o programa!!")
         break
+
     else:
         print(f"opção {user_op} invalida! Escolha uma opção valida.")      
