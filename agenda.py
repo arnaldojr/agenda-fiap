93f124ecfbfbb1d5d828c0d26162c78e94e691ad
arnaldojr <arnaldo_viana@yahoo.com.br>
2023-11-01 13:47:50 -0300

    estrutura e legibilidade

--- agenda.py
+++ agenda.py
@@ -1,49 +1,52 @@
+import re
+import json
 
-""""
-o sistema vai ter as funções
+def validar_email(email):
+    """Valida o formato do email."""
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(pattern, email) is not None
 
-adicionar contato
-listar contato
-editar contato
-excluir contato
-buscar contato
-
-a estrutura da agenda será um dicionario de dicionarios para cada contato.
-
-
-"""
-agenda = {
-    "pedro":{
-        "telefone":"22258-6998",
-        "email": "pedro@email.com",
-        "endereço": "rua pedro, 123"
-    },
-    "renato":{
-        "telefone":"9999666332",
-        "email": "renato@email.com",
-        "endereço": "rua renato, 123"
-    } 
-}
+def validar_telefone(telefone):
+    """Valida o formato do telefone."""
+    pattern = r'^\d{2}-\d{8,9}$'
+    return re.match(pattern, telefone) is not None
 
 def adicionar_contato(agenda,nome,tel,email,end):
+    """Adiciona um novo contato à agenda."""
+    if not validar_telefone(tel):
+        print("Telefone inválido. O formato correto é 'XX-XXXXXXXXX'.")
+        return
+    if not validar_email(email):
+        print("Email inválido.")
+        return
+
     novo_contato = {}
     novo_contato["telefone"] = tel
     novo_contato["email"] = email
     novo_contato["endereço"] = end
     
     agenda[nome]= novo_contato
-    print(f"contato {nome} adicionado com sucesso!!!")
+    print(f"Contato {nome} adicionado com sucesso!!!")
 
 
 def listar_contato(agenda):
+    """Lista todos os contatos na agenda."""
     print(f"agenda possui {len(agenda)} contatos.")
     for nome, contato in agenda.items():
-        print(nome)
+        print(f"\nNome: {nome}")
         for tipo, valor in contato.items():
             print(f"{tipo} - {valor}")
 
 
 def editar_contato(agenda,nome,tel,email,end):
+    """Edita um contato existente na agenda."""
+    if not validar_telefone(tel):
+        print("Telefone inválido. O formato correto é 'XX-XXXXXXXXX'.")
+        return
+    if not validar_email(email):
+        print("Email inválido.")
+        return
+    
     novo_contato = {}
     novo_contato["telefone"] = tel
     novo_contato["email"] = email
@@ -53,20 +56,38 @@ def editar_contato(agenda,nome,tel,email,end):
     print(f"contato {nome} editado com sucesso!!!")
 
 def excluir_contato(agenda,nome):
-    #forma 1
+    """Exclui um contato existente na agenda."""
     if nome in agenda:
         del agenda[nome]
         print(f"contato {nome} excluido com sucesso. :)")
     else:
         print(f"Contato {nome} não existe!")
     # forma 2
-    #del agenda.get(nome, "não existe")
+    # del agenda.get(nome, "Contato não existe!")
 
 def buscar_contato(agenda, nome):
-    print(agenda.get(nome, "contato não existe"))
+    """Busca um contato na agenda pelo nome."""
+    contato = agenda.get(nome)
+    if contato:
+        print(f"\nNome: {nome}")
+        for tipo, valor in contato.items():
+            print(f"{tipo} - {valor}")
 
+def salvar_agenda(agenda):
+    """Salva a agenda em um arquivo JSON."""
+    with open('agenda.json', 'w') as f:
+        json.dump(agenda, f)
+
+def carregar_agenda():
+    """Carrega a agenda de um arquivo JSON."""
+    try:
+        with open('agenda.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
 
 def menu():
+    agenda = carregar_agenda()
     while True:
         print("\n__agenda de contatos__\n")
         print(" 1. - adicionar contato")
@@ -80,7 +101,7 @@ def menu():
         if user_op == "1":
             user_name = input("Nome do contato:")
             if user_name not in agenda:
-                user_tel = input("Tel do contato:")
+                user_tel = input("Tel do contato ('XX-XXXXXXXXX'):")
                 user_email = input("email do contato:")
                 user_end = input("end do contato:")
                 adicionar_contato(agenda,user_name,user_tel,user_email,user_end)
@@ -93,7 +114,7 @@ def menu():
         elif user_op == "3":
             user_name = input("Nome do contato:")
             if user_name in agenda:
-                user_tel = input("Tel do contato:")
+                user_tel = input("Tel do contato (XX-XXXXXXXXX):")
                 user_email = input("email do contato:")
                 user_end = input("end do contato:")
                 editar_contato(agenda,user_name,user_tel,user_email,user_end)
@@ -110,6 +131,7 @@ def menu():
 
         elif user_op == "6":
             print("Obrigado, fechando o programa!!")
+            salvar_agenda(agenda)
             break
 
         else:
